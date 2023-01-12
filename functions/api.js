f450e06955165aaa09f33ac34fa2003a1ed40188
Konstantin Zhirnov <zhirnov-11.06@yandex.ru>
2023-01-11 22:22:17 -0800

    Netlify

---
+++ functions/api.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const { graphqlHTTP } = require('express-graphql');
+const serverless = require('serverless-http');
+const cors = require('cors');
+const schema = require('./schema/schema');
+
+const app = express();
+const router = express.Router();
+
+const corsOptions = {
+  origin(origin, callback) {
+    callback(null, true);
+  },
+  credentials: true
+};
+// app.use(cors(corsOptions));
+// const allowCrossDomain = function(req, res, next) {
+//   res.header('Access-Control-Allow-Origin', '*');
+//   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+//   res.header('Access-Control-Allow-Headers', 'Content-Type,token');
+//   next();
+// }
+// app.use(allowCrossDomain);
+
+app.use('/', graphqlHTTP({
+  schema,
+  graphiql: true
+}));
+
+
+module.exports.handler = serverless(app);
