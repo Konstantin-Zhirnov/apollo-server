e22b43171c9c87b5bd971d0c89974a15e6d45963
Konstantin Zhirnov <zhirnov-11.06@yandex.ru>
2023-01-17 21:23:30 -0800

    fix

--- functions/api.js
+++ functions/api.js
@@ -1,25 +1,25 @@
 const express = require('express');
 const { graphqlHTTP } = require('express-graphql');
 const serverless = require('serverless-http');
-// const cors = require('cors');
+const cors = require('cors');
 const schema = require('./schema/schema');
 
 const app = express();
 
-// const corsOptions = {
-//   origin(origin, callback) {
-//     callback(null, true);
-//   },
-//   credentials: true
-// };
-// app.use(cors(corsOptions));
-// const allowCrossDomain = function(req, res, next) {
-//   res.header('Access-Control-Allow-Origin', '*');
-//   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-//   res.header('Access-Control-Allow-Headers', 'Content-Type,token');
-//   next();
-// }
-// app.use(allowCrossDomain);
+const corsOptions = {
+  origin(origin, callback) {
+    callback(null, true);
+  },
+  credentials: true
+};
+app.use(cors(corsOptions));
+const allowCrossDomain = function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type,token');
+  next();
+}
+app.use(allowCrossDomain);
 
 app.use('/', graphqlHTTP({
   schema,
